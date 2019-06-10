27398af2462194e4d56306cf179e1fa969a0db1b
Aditya Nain <adityanain.work@gmail.com>
2019-06-10 21:24:26 +0530

    combined files

--- C/linkedlist2.cpp
+++ C/linkedlist2.cpp
@@ -11,7 +11,34 @@ struct Node
 
 struct Node* head;
 
-void Insert(int x)
+void Insert(int x, int n)
+{
+	//Node* temp1 = (Node*)malloc(sizeof(struct Node))   ---> c
+	Node* temp1 = new Node(); //c++
+	temp1->data = x;
+	temp1->next = NULL;
+	if(n==1)
+	{
+		temp1->next = head;
+		head = temp1;
+		return;
+	}
+	Node* temp2 = head;
+	for(int i = 0;i<n-2;i++)
+	{
+		temp2 = temp2->next;
+	}
+	temp1->next = temp2->next;
+	temp2->next = temp1;
+}
+void InsertAtFront(int x)
+{
+		Node* temp = (Node*)malloc(sizeof(struct Node));
+		temp->data = x;
+		temp->next = head;
+		head = temp;
+}
+void InsertAtEnd(int x)
 {
 	//Node* temp = (Node*)malloc(sizeof(struct Node));
 	Node* temp = new Node();
@@ -94,16 +121,11 @@ void ReverseRecursively()
 int main()
 {
 	head = NULL;
-	Insert(1);
-	Insert(2);
-	Insert(3);
-	Insert(4);
-	Insert(5);
-	Print();
-	PrintRecursively(head);
-	Reverse();
-	Print();
-	ReverseRecursively();
+	InsertAtEnd(1);
+	InsertAtEnd(2);
+	InsertAtEnd(3);
+	InsertAtEnd(4);
+	InsertAtEnd(5);
 	Print();
 	
 }
